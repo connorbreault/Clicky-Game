d335fee9c5f3aee458967a0afb3fe5046409ca3b
Spaghetti Jones <connor.breault@comcast.net>
2019-10-23 01:14:04 -0700

    card css

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Friend from "./components/Friend";
+import Wrapper from "./components/Wrapper";
+import Navbar from "./components/Navbar";
+import Title from "./components/Title";
+import friends from "./friends.json";
 
-const App = () => {
-  return (
-    <p>Hi</p>
-  );
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Navbar />
+        <Title>Friends List</Title>
+        {this.state.friends.map(friend => (
+          <Friend
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
---
+++ clicky-game/src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+function Navbar(props) {
+    return (
+        <div className="nav navbar">
+        </div>
+    );
+}
+
+export default Navbar;
